7470dd344d6adfb37f2e4eaec0e0b587ef75ba68
Ayan Biswas <biswas.ayan1997@gmail.com>
2024-06-10 16:14:10 +0530

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,12 +1,10 @@
-
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
-
-
+from src.DimondPricePrediction.components.model_evalution import ModelEvaluation
 
 import os
 import sys
@@ -15,8 +13,18 @@ from src.DimondPricePrediction.exception import customexception
 import pandas as pd
 
 obj=DataIngestion()
+
 train_data_path,test_data_path=obj.initiate_data_ingestion()
+
 data_transformation=DataTransformation()
+
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
-model_trainer=ModelTrainer()
-model_trainer.initate_model_training(train_arr,test_arr)
+
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+    
