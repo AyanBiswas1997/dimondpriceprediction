ad61cb8c3d5bc97796a2af9b686d7c4adfafb47c
Ayan Biswas <biswas.ayan1997@gmail.com>
2024-06-11 13:34:04 +0530

    updated

--- src/DimondPricePrediction/components/model_evalution.py
+++ src/DimondPricePrediction/components/model_evalution.py
@@ -31,7 +31,7 @@ class ModelEvaluation:
 
         
 
-            mlflow.set_registry_uri("https://dagshub.com/AyanBiswas1997/dimondpriceprediction")
+            mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
             
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
             
