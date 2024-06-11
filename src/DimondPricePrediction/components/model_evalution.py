9fc88067a7ea358f71b6a7fc305a13b4b7aa562a
Ayan Biswas <biswas.ayan1997@gmail.com>
2024-06-11 13:38:55 +0530

    new added

--- src/DimondPricePrediction/components/model_evalution.py
+++ src/DimondPricePrediction/components/model_evalution.py
@@ -1,69 +1,69 @@
-import pandas as pd
-import numpy as np
 import os
 import sys
-from src.DimondPricePrediction.logger import logging
-from src.DimondPricePrediction.exception import customexception
-from dataclasses import dataclass
-from src.DimondPricePrediction.utils.utils import save_object
-from src.DimondPricePrediction.utils.utils import evaluate_model
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pickle
+from src.DimondPricePrediction.utils.utils import load_object
 
-from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 
 
-@dataclass 
-class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
-    
-    
-class ModelTrainer:
+class ModelEvaluation:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        pass
+
     
-    def initate_model_training(self,train_array,test_array):
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
+        mae = mean_absolute_error(actual, pred)# here is MAE
+        r2 = r2_score(actual, pred)# here is r3 value
+        return rmse, mae, r2
+
+
+    def initiate_model_evaluation(self,train_array,test_array):
         try:
-            logging.info('Splitting Dependent and Independent variables from train and test data')
-            X_train, y_train, X_test, y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
-            )
+            X_test,y_test=(test_array[:,:-1], test_array[:,-1])
 
-            models={
-            'LinearRegression':LinearRegression(),
-            'Lasso':Lasso(),
-            'Ridge':Ridge(),
-            'Elasticnet':ElasticNet()
-        }
-            
-            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
-            print(model_report)
-            print('\n====================================================================================\n')
-            logging.info(f'Model Report : {model_report}')
-
-            # To get best model score from dictionary 
-            best_model_score = max(sorted(model_report.values()))
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            
-            best_model = models[best_model_name]
-
-            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
-            print('\n====================================================================================\n')
-            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
-
-            save_object(
-                 file_path=self.model_trainer_config.trained_model_file_path,
-                 obj=best_model
-            )
-          
-
-        except Exception as e:
-            logging.info('Exception occured at Model Training')
-            raise customexception(e,sys)
+            model_path=os.path.join("artifacts","model.pkl")
+            model=load_object(model_path)
 
         
-    
+
+            mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
+            
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            
+            print(tracking_url_type_store)
+
+
+
+            with mlflow.start_run():
+
+                predicted_qualities = model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+
+
+                
+
+            
+        except Exception as e:
+            raise e
